050022160b74afebd58657fe2e682c8af851f7ac
mohammedaladhary <caladhary@gmail.com>
2023-11-07 22:03:37 +0300

    inshallah

---
+++ todo-list_app/src/app/components/todos/todos.component.ts
@@ -0,0 +1,89 @@
+import { Component } from '@angular/core';
+
+interface Todo {
+  content: string;
+  completed: boolean;
+  postponed: boolean;
+  editing: boolean;
+  completedClass?: string;
+}
+
+@Component({
+  selector: 'app-todos',
+  templateUrl: './todos.component.html',
+  styleUrls: ['./todos.component.css']
+})
+export class TodosComponent {
+  todos: Todo[] = [];
+  inputTodo = '';
+  errorMessage: string = '';
+
+  toggleDone(id: number) {
+    this.todos[id].completed = !this.todos[id].completed;
+    this.todos[id].completedClass = this.todos[id].completed ? 'completed' : '';
+  }
+
+  deleteTodo(id: number) {
+    this.todos.splice(id, 1);
+  }
+
+  markAsDone(id: number) {
+    this.toggleDone(id);
+  }
+
+  postponeTodo(id: number) {
+    // Toggle the postponed state of the todo
+    this.todos[id].postponed = !this.todos[id].postponed;
+  }
+
+  cleanCompleted() {
+    this.todos = this.todos.filter((todo) => !todo.completed);
+  }
+
+  restorePostponed() {
+    // Set the postponed state to false for all todos
+    this.todos.forEach((todo) => (todo.postponed = false));
+  }
+
+  editTodo(id: number) {
+    this.todos[id].editing = true;
+  }
+
+  saveTodo(id: number) {
+    const updatedContent = this.todos[id].content.trim();
+  
+    if (updatedContent === '') {
+      this.errorMessage = 'Please enter a to-do name.';
+    } else if (this.todos.some((todo, index) => index !== id && todo.content === updatedContent)) {
+      this.errorMessage = 'A to-do with the same name already exists.';
+    } else {
+      this.todos[id].editing = false;
+      this.errorMessage = ''; // Clear the error message
+    }
+  }  
+
+  cancelEdit(id: number) {
+    this.todos[id].editing = false;
+  }
+
+  addTodo() {
+    const newTodoContent = this.inputTodo.trim();
+  
+    if (newTodoContent === '') {
+      this.errorMessage = 'Please enter a to-do name.';
+    } else if (this.todos.some((todo) => todo.content === newTodoContent)) {
+      this.errorMessage = 'A to-do with the same name already exists.';
+    } else {
+      this.todos.push({
+        content: newTodoContent,
+        completed: false,
+        postponed: false,
+        editing: false,
+        completedClass: ''
+      });
+  
+      this.inputTodo = '';
+      this.errorMessage = ''; // Clear the error message
+    }
+  }  
+}
